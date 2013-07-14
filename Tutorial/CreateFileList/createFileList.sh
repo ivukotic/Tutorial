66cd350c391500101053f36681b3789add635350
Ilija Vukotic <ivukotic@uct3-s1.uchicago.edu>
2013-07-14 12:14:25 -0500

    made --quiet

--- Tutorial/CreateFileList/createFileList.sh
+++ Tutorial/CreateFileList/createFileList.sh
@@ -2,6 +2,35 @@ source ../getDQ2.sh
 export STORAGEPREFIX=root://fax.mwt2.org:1094/
 dq2-list-files -p user.ilijav.HCtest.1 > ../ilijav_files.txt
 dq2-list-files -p user.flegger.MWT2.data12_8TeV.00212172.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007411_00 > ../MWT2_files.txt
-dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00212172.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007411_00 > ../AGLT2_files.txt
-dq2-list-files -p user.flegger.WT2.data12_8TeV.00212172.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007411_00 > ../WT2_files.txt
+dq2-list-files -p user.flegger.MWT2.data12_8TeV.00211697.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00987986_00 >> ../MWT2_files.txt
+dq2-list-files -p user.flegger.MWT2.data12_8TeV.00211620.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00986521_00 >> ../MWT2_files.txt
+dq2-list-files -p user.flegger.MWT2.data12_8TeV.00211522.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00986520_00 >> ../MWT2_files.txt
+dq2-list-files -p user.flegger.MWT2.data12_8TeV.00212144.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00999023_00 >> ../MWT2_files.txt
+dq2-list-files -p user.flegger.MWT2.data12_8TeV.00211937.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00994157_00 >> ../MWT2_files.txt
+dq2-list-files -p user.flegger.MWT2.data12_8TeV.00212000.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00994158_00 >> ../MWT2_files.txt
+dq2-list-files -p user.flegger.MWT2.data12_8TeV.00212199.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007410_00 >> ../MWT2_files.txt
+dq2-list-files -p user.flegger.MWT2.data12_8TeV.00211772.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00990030_00 >> ../MWT2_files.txt
+dq2-list-files -p user.flegger.MWT2.data12_8TeV.00211787.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00990029_00 >> ../MWT2_files.txt
+
+dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00212172.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007411_00 >  ../AGLT2_files.txt
+dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00211697.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00987986_00 >> ../AGLT2_files.txt
+dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00211620.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00986521_00 >> ../AGLT2_files.txt
+dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00211522.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00986520_00 >> ../AGLT2_files.txt
+dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00212144.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00999023_00 >> ../AGLT2_files.txt
+dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00211937.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00994157_00 >> ../AGLT2_files.txt
+dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00212000.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00994158_00 >> ../AGLT2_files.txt
+dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00212199.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007410_00 >> ../AGLT2_files.txt
+dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00211772.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00990030_00 >> ../AGLT2_files.txt
+dq2-list-files -p user.flegger.AGLT2.data12_8TeV.00211787.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00990029_00 >> ../AGLT2_files.txt
+
+dq2-list-files -p user.flegger.WT2.data12_8TeV.00212172.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007411_00 >  ../WT2_files.txt
+dq2-list-files -p user.flegger.WT2.data12_8TeV.00211697.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00987986_00 >> ../WT2_files.txt
+dq2-list-files -p user.flegger.WT2.data12_8TeV.00211620.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00986521_00 >> ../WT2_files.txt
+dq2-list-files -p user.flegger.WT2.data12_8TeV.00211522.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00986520_00 >> ../WT2_files.txt
+dq2-list-files -p user.flegger.WT2.data12_8TeV.00212144.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00999023_00 >> ../WT2_files.txt
+dq2-list-files -p user.flegger.WT2.data12_8TeV.00211937.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00994157_00 >> ../WT2_files.txt
+dq2-list-files -p user.flegger.WT2.data12_8TeV.00212000.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00994158_00 >> ../WT2_files.txt
+dq2-list-files -p user.flegger.WT2.data12_8TeV.00212199.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007410_00 >> ../WT2_files.txt
+dq2-list-files -p user.flegger.WT2.data12_8TeV.00211772.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00990030_00 >> ../WT2_files.txt
+dq2-list-files -p user.flegger.WT2.data12_8TeV.00211787.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00990029_00 >> ../WT2_files.txt
 
