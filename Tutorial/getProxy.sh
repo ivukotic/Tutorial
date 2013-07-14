ea72b3112fcb8c3d8fb893fd42d8971ce5cc673b
Ilija Vukotic <ivukotic@uct3-s1.uchicago.edu>
2013-07-14 12:23:18 -0500

    made quiet

--- Tutorial/getProxy.sh
+++ Tutorial/getProxy.sh
@@ -1,4 +1,5 @@
+ ##################  getProxy.sh  #################
  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
- source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+ source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh --quiet
  localSetupGLite
  voms-proxy-init -voms atlas
