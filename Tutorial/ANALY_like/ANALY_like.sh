43f53bef8a7560b614ff88a24dfa5fe931e0f538
Ilija Vukotic <ivukotic@cern.ch>
2013-07-15 16:18:03 -0500

    moved to slc6

--- Tutorial/ANALY_like/ANALY_like.sh
+++ Tutorial/ANALY_like/ANALY_like.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 A=$PWD
 echo "Setting up gcc"
-source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/Gcc/current/setup.sh
+source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/Gcc/gcc472_x86_64_slc6/setup.sh
 echo "Setting up ROOT"
-cd /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/root/5.34.07-x86_64-slc5-gcc4.3/
+cd /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/root/5.34.09-x86_64-slc6-gcc4.7/
 source bin/thisroot.sh
 echo "Setting up xRootD"
-source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/root/5.34.07-x86_64-slc5-gcc4.3/bin/setxrd.sh /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/xrootd/3.2.4-x86_64-slc5/v3.2.4/ 2>&1 > /dev/null
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/xrootd/3.2.4-x86_64-slc5/v3.2.4/lib
+source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/root/5.34.09-x86_64-slc6-gcc4.7/bin/setxrd.sh /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/xrootd/3.2.7-x86_64-slc6/v3.2.7/ 2>&1 > /dev/null
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/xrootd/3.2.7-x86_64-slc6/v3.2.7/lib
 cd $A
 
 export X509_USER_PROXY=x509up_u20074
