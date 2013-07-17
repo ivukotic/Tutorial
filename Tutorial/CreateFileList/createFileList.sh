52f012c13ed5fda2dfbe4c1d091ef096cd98d13b
Ilija Vukotic <ivukotic@cern.ch>
2013-07-17 09:12:27 -0500

    forgot to remove that #

--- Tutorial/CreateFileList/createFileList.sh
+++ Tutorial/CreateFileList/createFileList.sh
@@ -1,7 +1,7 @@
 source ../getDQ2.sh
 export STORAGEPREFIX=root://fax.mwt2.org:1094/
 dq2-list-files -p user.ilijav.HCtest.1 > ../ilijav_files.txt
-#dq2-list-files -p user.flegger.MWT2.data12_8TeV.00212172.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007411_00 > ../MWT2_files.txt
+dq2-list-files -p user.flegger.MWT2.data12_8TeV.00212172.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007411_00 > ../MWT2_files.txt
 #dq2-list-files -p user.flegger.MWT2.data12_8TeV.00211697.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00987986_00 >> ../MWT2_files.txt
 #dq2-list-files -p user.flegger.MWT2.data12_8TeV.00211620.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00986521_00 >> ../MWT2_files.txt
 #dq2-list-files -p user.flegger.MWT2.data12_8TeV.00211522.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid00986520_00 >> ../MWT2_files.txt
